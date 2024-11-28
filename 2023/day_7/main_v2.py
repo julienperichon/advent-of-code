990d25d3a8eff73788787688144d52701a7f486d
Julien Perichon <julienperichon023@gmail.com>
2024-11-28 01:38:56 +0100

    Add solution for 2023 day 7, part 2

--- 2023/day_7/main_v2.py
+++ 2023/day_7/main_v2.py
@@ -4,7 +4,7 @@ KK677 28
 KTJJT 220
 QQQJA 483"""
 
-CARD_ORDER = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+CARD_ORDER = ["J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A"]
 
 
 class Hand:
@@ -23,14 +23,17 @@ class Hand:
         hand_counts = {}
         for card in hand:
             hand_counts[card] = hand_counts.get(card, 0) + 1
-        hand_counts_values = list(hand_counts.values())
-        match max(hand_counts_values):
+        hand_counts_without_joker = {
+            key: value for key, value in hand_counts.items() if key != "J"
+        }
+        hand_counts_values = list(hand_counts_without_joker.values())
+        match max(hand_counts_values, default=0) + hand_counts.get("J", 0):
             case 1:
                 return 0
             case 2:
-                return 1 if len(hand_counts) == 4 else 2
+                return 1 if len(hand_counts_without_joker) == 4 else 2
             case 3:
-                return 3 if len(hand_counts) == 3 else 4
+                return 3 if len(hand_counts_without_joker) == 3 else 4
             case 4:
                 return 5
             case 5:
@@ -51,6 +54,7 @@ def process_input(input_txt: str) -> list[Hand]:
 
 with open("input.txt", "r") as input_file:
     input_data = input_file.read()
+# input_data = test_input
 processed_input = process_input(input_data)
 sorted_hands = sorted(processed_input, key=lambda h: h.hand_value)
 total_bid_won = 0
