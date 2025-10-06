3aecdb7fb805c2825fac1eb0744e232f0295c095
Julien Perichon <julien.perichon@theodo.com>
2025-10-07 00:09:28 +0200

    Optimize previous solution using dynamic programming and memoization

--- 2023/day_12_v1.py
+++ 2023/day_12_v1.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 from utils import get_input_data
 
 
@@ -9,41 +11,62 @@ test_input = """???.### 1,1,3
 ?###???????? 3,2,1"""
 
 
-def process_input(input_data: str) -> tuple[list[str], list[list[int]]]:
+def process_input(input_data: str) -> tuple[list[str], list[tuple[int, ...]]]:
     spring_data: list[str] = []
-    arrangement_data: list[list[int]] = []
+    arrangement_data: list[tuple[int, ...]] = []
     for row in input_data.rstrip().split("\n"):
         springs, arrangement = row.split(" ")
         spring_data.append(springs)
-        arrangement_data.append([int(x) for x in arrangement.split(",")])
+        arrangement_data.append(tuple([int(x) for x in arrangement.split(",")]))
     return (spring_data, arrangement_data)
 
 
-def compute_active_arrangement(spring_row: str) -> list[int]:
-    return list(filter(lambda x: x > 0, map(len, spring_row.split("."))))
+def subproblem_from_hole(spring_data: str, target_arrangement: tuple[int, ...]) -> int:
+    return compute_row_valid_arrangements(spring_data[1:], target_arrangement)
 
 
-def compute_row_valid_arrangements(
-    spring_data: str, target_arrangement: list[int]
+def subproblem_from_spring(
+    spring_data: str, target_arrangement: tuple[int, ...]
 ) -> int:
-    valid_arrangements = [""]
-    for char in spring_data:
-        if char == "?":
-            valid_arrangements = [
-                *[s + "#" for s in valid_arrangements],
-                *[s + "." for s in valid_arrangements],
-            ]
-        else:
-            valid_arrangements = [s + char for s in valid_arrangements]
+    spring_group = spring_data[: target_arrangement[0]]
+    if "." in spring_group:
+        return 0
+    if (
+        len(spring_data) > target_arrangement[0]
+        and spring_data[target_arrangement[0]] == "#"
+    ):
+        return 0
 
-    total_target_arrangements = 0
-    for arrangement in valid_arrangements:
-        total_target_arrangements += (
-            compute_active_arrangement(arrangement) == target_arrangement
-        )
-    return total_target_arrangements
+    return compute_row_valid_arrangements(
+        spring_data[target_arrangement[0] + 1 :], target_arrangement[1:]
+    )
 
 
+@cache
+def compute_row_valid_arrangements(
+    spring_data: str, target_arrangement: tuple[int, ...]
+) -> int:
+    if len(spring_data) < sum(target_arrangement) + len(target_arrangement) - 1:
+        return 0
+    if len(target_arrangement) == 0:
+        if "#" in spring_data:
+            return 0
+        return 1
+    if len(spring_data) == 0:
+        return 0
+
+    current_char = spring_data[0]
+    if current_char == ".":
+        return subproblem_from_hole(spring_data, target_arrangement)
+    if current_char == "#":
+        return subproblem_from_spring(spring_data, target_arrangement)
+
+    return subproblem_from_hole(
+        spring_data, target_arrangement
+    ) + subproblem_from_spring(spring_data, target_arrangement)
+
+
+# input_data = test_input
 input_data = get_input_data("2023_day_12.txt")
 spring_data, arrangement_data = process_input(input_data)
 total_score = 0
